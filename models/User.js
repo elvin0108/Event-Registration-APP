54bd37c883f4c713261b1909d28725cc84f4c24c
Elvin Khunt <68266019+elvin0108@users.noreply.github.com>
2024-02-21 23:13:11 +0530

    Added std and school in model

--- models/User.js
+++ models/User.js
@@ -4,7 +4,9 @@ const userSchema = new mongoose.Schema({
   username: String,
   mobile: String,
   category: String,
-  village: String
+  village: String,
+  std: String,
+  school: String
 });
 
 const User = mongoose.model('visitors', userSchema);
